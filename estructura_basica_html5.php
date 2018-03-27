5ee5fe871a753e7b42f3dc250a03eff214774101
[eduardorocal40] <[eduardorocal40@gmail.com]>
2018-03-27 01:31:11 -0600

    Signed-off-by: [eduardorocal40] <[eduardorocal40@gmail.com]>

---
+++ estructura_basica_html5.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>HTML5</title>
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+</script>
+<![endif]-->
+<style>
+html {
+    width:100%;
+    height: 100%;
+    display: table;
+}
+
+body {
+    width:100%;
+    display:table-cell;
+}
+
+html, body {
+    margin: 0px;
+    padding: 0px;
+}
+
+body {
+    font-family: Verdana,sans-serif;
+    font-size: 0.9em;
+}
+
+header, footer {
+    padding: 10px;
+    color: white;
+    background-color: black;
+}
+
+section {
+    margin: 5px;
+    padding: 10px;
+    background-color: lightgrey;
+}
+
+article {
+    margin: 5px;
+    padding: 10px;
+    background-color: white;
+}
+
+nav ul {
+    padding: 0;
+}
+
+nav ul li {
+    display: inline;
+    margin: 5px;
+}
+</style>
+</head>
+<body>
+
+<header>
+<h1>Monday Times</h1>
+</header>
+
+<nav>
+<ul>
+  <li>News</li>
+  <li>Sports</li>
+  <li>Weather</li>
+  <li>Weather</li>
+  <li>Weather</li>
+  <li>Weather</li>
+  <li>Weather</li>
+  <li>Weather</li>
+</ul>
+</nav>
+
+<section>
+
+<h2>News Section</h2>
+<article>
+<h2>News Article</h2>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in porta lorem. Morbi condimentum est nibh, et consectetur tortor feugiat at.</p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in porta lorem. Morbi condimentum est nibh, et consectetur tortor feugiat at.</p>
+</article>
+
+<article>
+<h2>News Article</h2>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in porta lorem. Morbi condimentum est nibh, et consectetur tortor feugiat at.</p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in porta lorem. Morbi condimentum est nibh, et consectetur tortor feugiat at.</p>
+</article>
+<aside>
+<h2>Aside section</h2>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in porta lorem. Morbi condimentum est nibh, et consectetur tortor feugiat at.</p>
+  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in porta lorem. Morbi condimentum est nibh, et consectetur tortor feugiat at.</p>
+</aside>
+
+<figure>
+<h2>Image section</h2>
+<img src="./pic_mountain.jpg" alt="The Pulpit Rock" width="304" height="228">
+<figcaption>Fig1. - The Pulpit Rock, Norway.</figcaption>
+</figure> 
+
+</section>
+
+<footer>
+<p>&copy; 2014 Monday Times. All rights reserved.</p>
+</footer>
+
+</body>
+</html>
